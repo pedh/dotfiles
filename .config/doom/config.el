7b04c9a4383a3c17f661869c901b87974a839dca
pedh <hcn518@gmail.com>
2023-11-06 14:32:18 +0800

    use doom-symbol-font variable
    Signed-off-by: pedh <hcn518@gmail.com>

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -37,7 +37,7 @@
   doom-font (font-spec :family "Monaco" :size 14)
   doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 14)
   doom-big-font (font-spec :family "Monaco" :size 20)
-  doom-unicode-font (font-spec :family "MesloLGS Nerd Font" :size 14)
+  doom-symbol-font (font-spec :family "MesloLGS Nerd Font" :size 14)
   doom-serif-font (font-spec :family "DejaVu Serif" :size 14))
 
 ;; Change the doom dashboard banner
